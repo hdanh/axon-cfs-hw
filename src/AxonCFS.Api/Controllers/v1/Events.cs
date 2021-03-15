5535fdc627e20e0ae984c95d8f520543d14be144
Danh Hoang <danh.hoang1991@gmail.com>
2021-03-15 23:16:28 +0700

    init setup

---
+++ src/AxonCFS.Api/Controllers/v1/Events.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AxonCFS.Api.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class Events : ControllerBase
+    {
+    }
+}
